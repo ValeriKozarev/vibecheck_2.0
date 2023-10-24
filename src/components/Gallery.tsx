04244e876ce187b612f09e9fb3acee113d03b28c
ValeriKozarev <val.kozarev.20@gmail.com>
2023-10-24 17:39:32 -0600

    rough POC for fetching and summarizing audio features

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -5,6 +5,8 @@ import './Gallery.css'
 interface ISongGroup {
   name: string
   images: any[]
+  id: string
+  isAlbum?: boolean
 }
 
 interface IGalleryProps {
@@ -36,9 +38,57 @@ class Gallery extends Component<IGalleryProps, IGalleryState> {
     this.setState({songGroups: songGroupArray})
   }
 
-  getAudioFeatures(album: ISongGroup) {
-    console.log('testing spongebob!')
-    console.log(album)
+  async getAudioFeatures(songGroup: ISongGroup) {
+    let searchParams = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.props.accessToken
+      }
+    }
+    let isAlbum = false;
+    
+    // step 1: determine if its an album or a playlist so we hit the right API
+    if ('album_type' in songGroup) {
+      if (songGroup.album_type === "album") {
+        isAlbum = true;
+      }
+    }
+
+    // step 2: get the list of tracks for the album or playlist (different endpoints same logic)
+    // https://api.spotify.com/v1/playlists/{playlist_id}/tracks
+    // https://api.spotify.com/v1/albums/{id}/tracks
+    let songData: string[] = []
+
+    if (isAlbum) {
+      songData = await fetch('https://api.spotify.com/v1/albums/' + songGroup.id + '/tracks', searchParams)
+      .then(result => result.json())
+      .then(data => {
+        return data.items.map((item: { id: string; }) => item.id)
+      })
+    } else {
+      songData = await fetch('https://api.spotify.com/v1/playlists/' + songGroup.id + '/tracks', searchParams)
+      .then(result => result.json())
+      .then(data => {
+        return data.items.map((item: { track: any; }) => item.track.id)
+      })      
+    }
+
+    // step 3: get the audio features for the list of tracks 
+    // https://api.spotify.com/v1/audio-features?ids=<comma separated list of IDs>
+    let audioFeatures = await fetch('https://api.spotify.com/v1/audio-features?ids=' + songData.toString(), searchParams)
+      .then(result => result.json())
+      .then(data => { return data.audio_features })
+
+    // step 4: parse/analyze audio features
+    console.log('Summary Stats for ' + songGroup.name)
+    console.log('avg energy: ' + (audioFeatures.reduce((total: number, next: any) => total + next.energy, 0) / audioFeatures.length))
+    console.log('avg danceability: ' + (audioFeatures.reduce((total: number, next: any) => total + next.danceability, 0) / audioFeatures.length))
+    console.log('avg acousticness: ' + (audioFeatures.reduce((total: number, next: any) => total + next.acousticness, 0) / audioFeatures.length))
+    console.log('avg instrumentalness: ' + (audioFeatures.reduce((total: number, next: any) => total + next.instrumentalness, 0) / audioFeatures.length))
+
+
+    // step 5: get recommendations? theres an aPI for that
   }
   
   // helper function for shuffling our array
@@ -62,8 +112,12 @@ class Gallery extends Component<IGalleryProps, IGalleryState> {
     await fetch('https://api.spotify.com/v1/search?q=' + this.state.searchInput + '&type=album,playlist', searchParams)
        .then(result => result.json())
        .then(data => { 
+        // build out one big array of albums and playlists and shuffle it
         let resArr = data.albums.items.concat(data.playlists.items)
         this.shuffleArray(resArr)
+
+        console.log(resArr)
+
         this.setSongGroups(resArr)
       })
   }
