39b809055cadf2791e8ab3df7e91ce85164589a2
ValeriKozarev <val.kozarev.20@gmail.com>
2024-02-07 11:07:08 -0700

    updating auth logic to use browser storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import {CLIENT_ID, REDIRECT_URI, AUTH_ENDPOINT, RESPONSE_TYPE} from './keys'
 import Gallery from './components/Gallery'
-import spotify_auth from './helpers/spotify_auth';
 
 import './App.css';
 
 function App() {
-  // data we will want to keep track of in the session
-  const [accessToken, setAccessToken] = useState("")
+  // token for our session with the Spotify APIs that we want to keep in state
+  const [token, setToken] = useState("")
 
   // NOTE: runs once at start to handle spotify auth
   useEffect(() => {
-    const receivedAccessToken: string = spotify_auth();
-    setAccessToken(receivedAccessToken)
+    const hash = window.location.hash
+    let token: any = window.localStorage.getItem("token")
+
+    if (!token && hash) {
+      token = hash.substring(1).split('&').find((elem) => elem.startsWith("access_token"))?.split("=")[1]
+
+      window.location.hash = ""
+      window.localStorage.setItem("token", token)
+      setToken(token)
+    }
+
   }, [])
+
+  // configure our URL for authenticating with Spotify
+  const spotify_auth_url = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`
 
   return (
     <div className="App">
-      <Gallery accessToken={accessToken}/>
+      <h1>Vibecheck 2.0</h1>
+      {!token ? 
+        <a href={spotify_auth_url}>Login to Spotify</a>
+        :
+        <Gallery accessToken={token}/>
+      }
     </div>
   );
 }
